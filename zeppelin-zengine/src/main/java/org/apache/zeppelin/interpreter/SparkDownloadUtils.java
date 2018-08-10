f47a3c5a92846c518567b2f1d7f6e84f908f0aa2
Jeff Zhang <zjffdu@apache.org>
2018-08-10 21:13:14 +0800

    [HOTIFX] Fix spark download failure

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/interpreter/SparkDownloadUtils.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/interpreter/SparkDownloadUtils.java
@@ -36,38 +36,21 @@ public class SparkDownloadUtils {
       LOGGER.info("Skip to download spark as it is already downloaded.");
       return targetSparkHomeFolder.getAbsolutePath();
     }
-    // Try mirrors a few times until one succeeds
-    boolean downloaded = false;
-    for (int i = 0; i < 3; i++) {
-      try {
-        String preferredMirror = IOUtils.toString(new URL("https://www.apache.org/dyn/closer.lua?preferred=true"));
-        File downloadFile = new File(downloadFolder + "/spark-" + version + "-bin-hadoop2.6.tgz");
-        String downloadURL = preferredMirror + "/spark/spark-" + version + "/spark-" + version + "-bin-hadoop2.6.tgz";
-        runShellCommand(new String[] {"wget", downloadURL, "-P", downloadFolder});
-        runShellCommand(new String[]{"tar", "-xvf", downloadFile.getAbsolutePath(), "-C", downloadFolder});
-        downloaded = true;
-        break;
-      } catch (Exception e) {
-        LOGGER.warn("Failed to download Spark", e);
-      }
-    }
-    // fallback to use apache archive
+    // use apache archive
     // https://archive.apache.org/dist/spark/spark-1.6.3/spark-1.6.3-bin-hadoop2.6.tgz
-    if (!downloaded) {
-      File downloadFile = new File(downloadFolder + "/spark-" + version + "-bin-hadoop2.6.tgz");
-      String downloadURL =
-          "https://archive.apache.org/dist/spark/spark-"
-              + version
-              + "/spark-"
-              + version
-              + "-bin-hadoop2.6.tgz";
-      try {
-        runShellCommand(new String[] {"wget", downloadURL, "-P", downloadFolder});
-        runShellCommand(
-            new String[] {"tar", "-xvf", downloadFile.getAbsolutePath(), "-C", downloadFolder});
-      } catch (Exception e) {
-        throw new RuntimeException("Fail to download spark " + version, e);
-      }
+    File downloadFile = new File(downloadFolder + "/spark-" + version + "-bin-hadoop2.6.tgz");
+    String downloadURL =
+        "https://archive.apache.org/dist/spark/spark-"
+            + version
+            + "/spark-"
+            + version
+            + "-bin-hadoop2.6.tgz";
+    try {
+      runShellCommand(new String[] {"wget", downloadURL, "-P", downloadFolder});
+      runShellCommand(
+          new String[] {"tar", "-xvf", downloadFile.getAbsolutePath(), "-C", downloadFolder});
+    } catch (Exception e) {
+      throw new RuntimeException("Fail to download spark " + version, e);
     }
     return targetSparkHomeFolder.getAbsolutePath();
   }
